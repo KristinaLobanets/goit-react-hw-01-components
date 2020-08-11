a77236ff7a62769b0a8bd967959de295c8ebbfcc
Kristina Lobanets <kristina.malitowska@gmail.com>
2020-08-11 20:39:46 +0300

    Updates

---
+++ precache-manifest.5284ca9639614b6d170c90574bd48225.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cd450be73eb75383db1c677c2959dc0",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "a4635734f5cb657cea90",
+    "url": "/goit-react-hw-01-components/static/css/main.bcb6c294.chunk.css"
+  },
+  {
+    "revision": "51fc9cf1963c89b96458",
+    "url": "/goit-react-hw-01-components/static/js/2.9da932c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.9da932c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4635734f5cb657cea90",
+    "url": "/goit-react-hw-01-components/static/js/main.e1fec26a.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
